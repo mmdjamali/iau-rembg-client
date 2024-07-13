8764db81a95470043a5368dd28e3c3c2d9a8e12a
MmD <mmdj27634@gmail.com>
2024-07-13 13:18:44 +0330

    add : dialog and icon component

---
+++ src/components/ui/dialog.tsx
@@ -0,0 +1,25 @@
+import * as DialogPrimitives from "@radix-ui/react-dialog";
+
+const Dialog = DialogPrimitives.Root;
+
+const DialogTrigger = DialogPrimitives.Trigger;
+
+const DialogPortal = DialogPrimitives.Portal;
+
+const DialogContent = DialogPrimitives.Content;
+
+const DialogOverlay = DialogPrimitives.Overlay;
+
+const DialogTitle = DialogPrimitives.Title;
+
+const DialogClose = DialogPrimitives.Close;
+
+export {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTrigger,
+  DialogTitle,
+  DialogClose,
+};
